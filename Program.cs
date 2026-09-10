33372964a013567492f04e3b4cfcaea01b9a38f1
xDosva <oddynucleth@gamil.com>
2026-09-09 20:28:37 -0600

    Juego para adivinar el número con contador de vida

--- Program.cs
+++ Program.cs
@@ -1,1 +1,54 @@
-﻿
+﻿namespace AdivinaNumero
+{
+    public class AdivinaNumero
+    {
+        public static int Numero {get;set;}
+        public static int Contador {get;set;}
+        public static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int NumeroAleatorio = rnd.Next(1,101);
+            Contador = 10;
+            Console.WriteLine("ADIVINA EL NÚMERO");
+            Console.WriteLine("-----------------");
+
+            while(Contador > 0)
+            {
+                try
+                {
+                    Console.Write("Numero: ");
+                    Numero = Convert.ToInt32(Console.ReadLine());
+                    if(Numero > NumeroAleatorio)
+                    {
+                        Console.WriteLine("El número es MENOR");
+                        Contador --;
+                        Console.WriteLine($"Vida: {Contador} \n");
+                    }
+
+                    else if(Numero < NumeroAleatorio)
+                    {
+                        Console.WriteLine("El número es MAYOR");
+                        Contador --;
+                        Console.WriteLine($"Vida: {Contador} \n");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("¡LE ATINASTE!");
+                        break;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("ERROR: Escoge un número válido \n");
+                }
+            }
+
+            if(Contador == 0)
+            {
+                Console.WriteLine($"PERDISTE JAJAJA el número era {NumeroAleatorio}");
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<h1>Adivina el Número (.NET Console)</h1>
 **Adivina el Número (.NET Console)**
 
 Juego interactivo de consola desarrollado en C# sobre la plataforma .NET. El programa genera un número aleatorio dentro de un rango determinado y guía al usuario para adivinarlo mediante pistas en tiempo real.
